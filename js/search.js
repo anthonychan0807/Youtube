ad7842be98fd8f5dd0a45d7bd8f03ec70c1d2627
anthonychan0807 <anthonychan@Anthonys-MacBook-Pro.local>
2014-11-22 22:09:35 -0800

    index.html
    <!DOCTYPE html>
    <html>
        <head>
            <script src="js/search2.js" type="text/javascript"></script>
            <script
    src="https://apis.google.com/js/client.js?onload=onClientLoad"
    type="text/javascript"></script>
        </head>
        <body>
            <pre id="response"></pre>
        </body>
    </html>
    
    <form id="searchbox" action="">
        <input id="search" type="text" placeholder="Type here">
        <input id="submit" type="submit" value="Search">
    </form>

---
+++ js/search.js
@@ -0,0 +1,57 @@
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) 
+{var responseString = JSON.stringify(response, '', 2);
+    var nodeObject = JSON.parse(responseString);
+    console.log(nodeObject);
+    var test = nodeObject.items[0];
+    console.log(test);
+
+            $.getJSON('https://gdata.youtube.com/feeds/api/videos?q=googledevelopers&max-re‌​sults=5&v=2&alt=jsonc&orderby=published', function(data) {
+    console.log(data);
+    for(var i=0; i<data.data.items.length; i++) 
+    {
+       console.log(data.data.items[i].title); // title
+       console.log(data.data.items[i].description); // description
+       console.log(data.data.items[i].default); //url link
+    }
+
+}
+
+    document.getElementById('response').innerHTML += responseString;
+}
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyAon54hwMdhITRKwPtc4-peI2LclwI5pR4');
+
+    search();
+}
+
+function search() {
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',   
+        q: ''
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
+    console.log(response);
+}
+{
+console.log("hello world");
+}
+
---
+++ js/search2.js
@@ -0,0 +1,44 @@
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+      var responseString = JSON.stringify(response, '', 2);
+    var nodeObject = JSON.parse(responseString);
+    console.log(nodeObject);
+    var test = nodeObject.items[0];
+    console.log(test);
+}
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See http://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyAon54hwMdhITRKwPtc4-peI2LclwI5pR4');
+
+    search();
+}
+
+function search() {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet'
+        q: 'kobe'
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
+}
